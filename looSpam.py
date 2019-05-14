de1ff4b36677767b4f5fefa659e43988af29eb2c
Tara Wilson <wwtara@hotmail.com>
2019-05-13 21:34:42 -0700

    Update looSpam.py
    Expanding to 1000 data points

--- looSpam.py
+++ looSpam.py
@@ -218,5 +218,5 @@ def leaveOneOutCrossValidation(X, y, lambdas, epsilon, rho):
 
 # leave one out cross validation
 lambdas = [0.0001, 0.001, 0.01, 0.1, 1]
-lOOLamb = leaveOneOutCrossValidation(X_train[0:300,:], y_train[0:300,:], lambdas, 0.001, 2)
+lOOLamb = leaveOneOutCrossValidation(X_train[0:1000,:], y_train[0:1000,:], lambdas, 0.001, 2)
 print(lOOLamb)
